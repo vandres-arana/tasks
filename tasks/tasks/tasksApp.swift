3df6f944034c16bc014d5bda14f50cc15a6ebbcd
Andres Arana <3av.arana@gmail.com>
2022-05-07 21:44:15 -0400

    Init project

---
+++ tasks/tasks/tasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  tasksApp.swift
+//  tasks
+//
+//  Created by Victor Arana on 5/7/22.
+//
+
+import SwiftUI
+
+@main
+struct tasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
